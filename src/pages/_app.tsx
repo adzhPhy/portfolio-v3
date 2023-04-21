a4440b144730c96eb12ee76c99c6d9115a2ea950
Ali-Dzhan Ali <adzh.aliali@gmail.com>
2023-04-21 11:54:11 +0300

    Added Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Providers from "@/helpers/Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
       <Component {...pageProps} />
+      <Analytics />
     </Providers>
   );
 }
